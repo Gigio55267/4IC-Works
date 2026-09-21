05a9cd482efc9d865962a9157731d455bee148e3
riccardo <riccardosaverino2009@gmail.com>
2026-09-21 09:45:44 +0200

    commit

--- PrimoThread.java
+++ PrimoThread.java
@@ -5,6 +5,7 @@ public class PrimoThread extends Thread{
     public void run() {
         for (char i = 'A'; i < 'Z'; i++) {
             System.out.print(i);
+            System.out.println(" sono il thread "+ getName());
         }
     }
      
--- SecondoThread.java
+++ SecondoThread.java
@@ -5,6 +5,7 @@ public class SecondoThread extends Thread{
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.print(i);
+            System.out.println(" sono il thread "+ getName());
         }
     }
     
